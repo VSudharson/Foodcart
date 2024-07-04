c875f48f53e56bf70441ef65d36d42233ca7ae1a
VSudharson <balajisudharson99@gmail.com>
2024-07-03 22:16:46 -0800

    first commit

---
+++ src/components/Viewcart.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState, useContext } from 'react';
+import './Viewcart.css';
+import { cartContext } from '../App';
+
+export const Viewcart = () => {
+  const { cart } = useContext(cartContext);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    setTotal(cart.reduce((acc,curr) => acc+parseInt(curr.amt), 0))
+  }, [cart]);
+  return (
+    <>
+    <h1 className='cart-headiing'>Cart Products</h1>
+    <div className='cart-container'>
+      {
+        cart.map((product) => (
+          <div className="cart-product" key={product.id}>
+            <div className="img">
+              <img src={product.pic} alt='image'/>
+            </div>
+            <div className="cart-product-details">
+              <h3>{product.name}</h3>
+              <p>Price Rs: {product.amt}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+    <h2 className='cart-amt'>Total Amount Rs: {total}</h2>
+    </>
+  );
+};
